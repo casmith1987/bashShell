bf71f36d82127d2f86109e9c1627bc39e05ae12a
casmith1987 <c.a.smith1987@gmail.com>
2020-09-25 12:11:48 -0500

    bash shell pwd and ls working

---
+++ bash.js
@@ -0,0 +1,12 @@
+const pwd = require('./pwd');
+const ls = require('./ls')
+
+process.stdout.write('prompt > ');
+process.stdin.on('data', (data)=> {
+  const cmd = data.toString().trim();
+  if (cmd === 'pwd') pwd();
+  if (cmd === 'ls') ls();
+
+  // process.stdout.write('You typed: ' + cmd);
+  // process.stdout.write('\nprompt > ');
+})
